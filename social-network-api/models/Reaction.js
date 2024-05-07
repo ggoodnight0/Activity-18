37868a603941bc3cca315cc7570087e89ac31532
ggoodnight0 <148833417+ggoodnight0@users.noreply.github.com>
2024-05-06 22:30:12 -0500

    Update Reaction.js

---
+++ social-network-api/models/Reaction.js
@@ -0,0 +1,11 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+module.exports = mongoose.model('User', userSchema);
